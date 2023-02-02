adcce419414f505b118a0cf73dd894de8f805286
Elena Dragun <edragun09@gmail.com>
2023-02-02 19:03:27 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listRef = document.querySelector(`#ingredients`);
+const ingredientRef = ingredients.map(element => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = element;
+  itemRef.classList.add('item');
+  return itemRef;
+
+});
+
+listRef.append(...ingredientRef);
+
+  console.log(itemRef);
+
+
