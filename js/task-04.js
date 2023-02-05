c0bed7afd58261f2ba3248b10ea4a344fb38bab2
Elena Dragun <edragun09@gmail.com>
2023-02-06 01:20:51 +0300

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,16 @@
-const decrementBtn = document.querySelector('[decrement]');
+const decrementBtn = document.querySelector('[data-action=decrement]');
 const valueRef = document.querySelector('#value');
-const incrementBtn = document.querySelector('[increment]');
+const incrementBtn = document.querySelector('[data-action=increment]');
+let counterValue = 0;
 
-decrementBtn.addEventListener('click', onDecrementClic);
-incrementBtn.addEventListener('click', onIncrementClic);
+decrementBtn.addEventListener('click', onDecrementClick);
+function onDecrementClick() {
+    counterValue -= 1;
+    valueRef.textContent = counterValue;
+};
+
+incrementBtn.addEventListener('click', onIncrementClick);
+function onIncrementClick() {
+    counterValue += 1;
+    valueRef.textContent = counterValue;
+}
